abacb94da4303209cf8ae06426a91561f622f197
bijoua <bijou.abraham@technipfmc.com>
2022-05-25 10:42:10 -0700

    Fix admittance rule for updated prototype of ControllerParameters
    Need to override update_storage() instead of update()
    Also lot of formatting changes

--- admittance_controller/include/admittance_controller/admittance_rule.hpp
+++ admittance_controller/include/admittance_controller/admittance_rule.hpp
@@ -19,29 +19,30 @@
 
 #include <map>
 
-#include "angles/angles.h"
+#include <tf2_ros/buffer.h>
 #include "admittance_controller/moveit_kinematics.hpp"
+#include "angles/angles.h"
 #include "control_msgs/msg/admittance_controller_state.hpp"
 #include "controller_interface/controller_interface.hpp"
 #include "controller_interface/controller_parameters.hpp"
 #include "filters/filter_chain.hpp"
-#include "geometry_msgs/msg/quaternion.hpp"
 #include "geometry_msgs/msg/pose_stamped.hpp"
+#include "geometry_msgs/msg/quaternion.hpp"
 #include "geometry_msgs/msg/transform_stamped.hpp"
 #include "geometry_msgs/msg/wrench_stamped.hpp"
 #include "rcutils/logging_macros.h"
-#include "tf2_ros/transform_listener.h"
 #include "tf2_geometry_msgs/tf2_geometry_msgs.hpp"
-#include <tf2_ros/buffer.h>
+#include "tf2_ros/transform_listener.h"
 
-namespace {  // Utility namespace
+namespace
+{  // Utility namespace
 
 // Numerical accuracy checks. Used as deadbands.
 static constexpr double WRENCH_EPSILON = 1e-10;
 static constexpr double POSE_ERROR_EPSILON = 1e-12;
 static constexpr double POSE_EPSILON = 1e-15;
 
-template<typename Type>
+template <typename Type>
 void convert_message_to_array(const geometry_msgs::msg::Pose & msg, Type & vector_out)
 {
   vector_out[0] = msg.position.x;
@@ -51,19 +52,19 @@ void convert_message_to_array(const geometry_msgs::msg::Pose & msg, Type & vecto
   tf2::fromMsg(msg.orientation, q);
   q.normalize();
   tf2::Matrix3x3(q).getRPY(vector_out[3], vector_out[4], vector_out[5]);
-  for (auto i = 3u; i < 6; ++i) {
+  for (auto i = 3u; i < 6; ++i)
+  {
     vector_out[i] = angles::normalize_angle(vector_out[i]);
   }
 }
 
-template<typename Type>
-void convert_message_to_array(
-  const geometry_msgs::msg::PoseStamped & msg, Type & vector_out)
+template <typename Type>
+void convert_message_to_array(const geometry_msgs::msg::PoseStamped & msg, Type & vector_out)
 {
   convert_message_to_array(msg.pose, vector_out);
 }
 
-template<typename Type>
+template <typename Type>
 void convert_message_to_array(const geometry_msgs::msg::Transform & msg, Type & vector_out)
 {
   vector_out[0] = msg.translation.x;
@@ -73,20 +74,20 @@ void convert_message_to_array(const geometry_msgs::msg::Transform & msg, Type & 
   tf2::fromMsg(msg.rotation, q);
   q.normalize();
   tf2::Matrix3x3(q).getRPY(vector_out[3], vector_out[4], vector_out[5]);
-  for (auto i = 3u; i < 6; ++i) {
+  for (auto i = 3u; i < 6; ++i)
+  {
     vector_out[i] = angles::normalize_angle(vector_out[i]);
   }
 }
 
-template<typename Type>
+template <typename Type>
 void convert_message_to_array(const geometry_msgs::msg::TransformStamped & msg, Type & vector_out)
 {
   convert_message_to_array(msg.transform, vector_out);
 }
 
-template<typename Type>
-void convert_message_to_array(
-  const geometry_msgs::msg::Wrench & msg, Type & vector_out)
+template <typename Type>
+void convert_message_to_array(const geometry_msgs::msg::Wrench & msg, Type & vector_out)
 {
   vector_out[0] = msg.force.x;
   vector_out[1] = msg.force.y;
@@ -96,14 +97,13 @@ void convert_message_to_array(
   vector_out[5] = msg.torque.z;
 }
 
-template<typename Type>
-void convert_message_to_array(
-  const geometry_msgs::msg::WrenchStamped & msg, Type & vector_out)
+template <typename Type>
+void convert_message_to_array(const geometry_msgs::msg::WrenchStamped & msg, Type & vector_out)
 {
   convert_message_to_array(msg.wrench, vector_out);
 }
 
-template<typename Type>
+template <typename Type>
 void convert_array_to_message(const Type & vector, geometry_msgs::msg::Pose & msg_out)
 {
   msg_out.position.x = vector[0];
@@ -119,7 +119,7 @@ void convert_array_to_message(const Type & vector, geometry_msgs::msg::Pose & ms
   msg_out.orientation.w = q.w();
 }
 
-template<typename Type>
+template <typename Type>
 void convert_array_to_message(const Type & vector, geometry_msgs::msg::PoseStamped & msg_out)
 {
   convert_array_to_message(vector, msg_out.pose);
@@ -142,7 +142,7 @@ void convert_array_to_message(const Type & vector, geometry_msgs::msg::PoseStamp
 //   convert_array_to_message(vector, msg_out.wrench);
 // }
 
-template<typename Type>
+template <typename Type>
 void convert_array_to_message(const Type & vector, geometry_msgs::msg::Transform & msg_out)
 {
   msg_out.translation.x = vector[0];
@@ -158,13 +158,13 @@ void convert_array_to_message(const Type & vector, geometry_msgs::msg::Transform
   msg_out.rotation.w = q.w();
 }
 
-template<typename Type>
+template <typename Type>
 void convert_array_to_message(const Type & vector, geometry_msgs::msg::TransformStamped & msg_out)
 {
   convert_array_to_message(vector, msg_out.transform);
 }
 
-}  // utility namespace
+}  // namespace
 
 namespace admittance_controller
 {
@@ -172,7 +172,7 @@ namespace admittance_controller
 class AdmittanceParameters : public controller_interface::ControllerParameters
 {
 public:
-  AdmittanceParameters() : controller_interface::ControllerParameters(7, 24, 4)
+  AdmittanceParameters() : controller_interface::ControllerParameters("", 7, 24, 4)
   {
     add_string_parameter("IK.base", false);
     add_string_parameter("IK.group_name", false);
@@ -224,7 +224,8 @@ public:
     int index = 0;
     int offset_index_bool = 1;
     // check if parameters are all properly set for selected axes
-    for (auto i = 0ul; i < 6; ++i) {
+    for (auto i = 0ul; i < 6; ++i)
+    {
       if (bool_parameters_[offset_index_bool + i].second)
       {
         // check mass parameters
@@ -232,8 +233,8 @@ public:
         if (std::isnan(double_parameters_[index].second))
         {
           RCUTILS_LOG_ERROR_NAMED(
-            logger_name_.c_str(),
-            "Parameter '%s' has to be set", double_parameters_[index].first.name.c_str());
+            logger_name_.c_str(), "Parameter '%s' has to be set",
+            double_parameters_[index].first.name.c_str());
           ret = false;
         }
         // Check stiffness parameters
@@ -241,21 +242,20 @@ public:
         if (std::isnan(double_parameters_[index].second))
         {
           RCUTILS_LOG_ERROR_NAMED(
-            logger_name_.c_str(),
-            "Parameter '%s' has to be set", double_parameters_[index].first.name.c_str());
+            logger_name_.c_str(), "Parameter '%s' has to be set",
+            double_parameters_[index].first.name.c_str());
           ret = false;
         }
         // Check damping or damping_ratio parameters
         index = i + 12;
-        if (std::isnan(double_parameters_[index].second) &&
-            std::isnan(double_parameters_[index + 6].second))
+        if (
+          std::isnan(double_parameters_[index].second) &&
+          std::isnan(double_parameters_[index + 6].second))
         {
           RCUTILS_LOG_ERROR_NAMED(
-            logger_name_.c_str(),
-            "Either parameter '%s' of '%s' has to be set",
+            logger_name_.c_str(), "Either parameter '%s' of '%s' has to be set",
             double_parameters_[index].first.name.c_str(),
-            double_parameters_[index + 6].first.name.c_str()
-          );
+            double_parameters_[index + 6].first.name.c_str());
           ret = false;
         }
       }
@@ -272,66 +272,51 @@ public:
   {
     for (auto i = 0ul; i < damping_ratio_.size(); ++i)
     {
-      if (!std::isnan( damping_ratio_[i]))
+      if (!std::isnan(damping_ratio_[i]))
       {
         damping_[i] = damping_ratio_[i] * 2 * sqrt(mass_[i] * stiffness_[i]);
       }
       else
       {
         RCUTILS_LOG_DEBUG_NAMED(
-          logger_name_.c_str(),
-          "Damping ratio for axis %zu not used because it is NaN.", i);
+          logger_name_.c_str(), "Damping ratio for axis %zu not used because it is NaN.", i);
       }
     }
   }
 
-  void update() override
+  void update_storage() override
   {
     ik_base_frame_ = string_parameters_[0].second;
-    RCUTILS_LOG_INFO_NAMED(
-        logger_name_.c_str(),
-       "IK Base frame: %s", ik_base_frame_.c_str());
+    RCUTILS_LOG_INFO_NAMED(logger_name_.c_str(), "IK Base frame: %s", ik_base_frame_.c_str());
     ik_group_name_ = string_parameters_[1].second;
-    RCUTILS_LOG_INFO_NAMED(
-        logger_name_.c_str(),
-       "IK group name frame: %s", ik_group_name_.c_str());
+    RCUTILS_LOG_INFO_NAMED(logger_name_.c_str(), "IK group name frame: %s", ik_group_name_.c_str());
     control_frame_ = string_parameters_[2].second;
-    RCUTILS_LOG_INFO_NAMED(
-        logger_name_.c_str(),
-       "Control frame: %s", control_frame_.c_str());
+    RCUTILS_LOG_INFO_NAMED(logger_name_.c_str(), "Control frame: %s", control_frame_.c_str());
     sensor_frame_ = string_parameters_[3].second;
-    RCUTILS_LOG_INFO_NAMED(
-        logger_name_.c_str(),
-       "Sensor frame: %s", sensor_frame_.c_str());
+    RCUTILS_LOG_INFO_NAMED(logger_name_.c_str(), "Sensor frame: %s", sensor_frame_.c_str());
 
     open_loop_control_ = bool_parameters_[0].second;
     RCUTILS_LOG_INFO_NAMED(
-        logger_name_.c_str(),
-       "Using open loop: %s", (open_loop_control_ ? "True" : "False"));
+      logger_name_.c_str(), "Using open loop: %s", (open_loop_control_ ? "True" : "False"));
 
     for (auto i = 0ul; i < 6; ++i)
     {
-      selected_axes_[i] = bool_parameters_[i+1].second; // +1 because there is already one parameter
+      selected_axes_[i] =
+        bool_parameters_[i + 1].second;  // +1 because there is already one parameter
       RCUTILS_LOG_INFO_NAMED(
-        logger_name_.c_str(),
-       "Axis %zu is %sselected", i, (selected_axes_[i] ? "" : "not "));
+        logger_name_.c_str(), "Axis %zu is %sselected", i, (selected_axes_[i] ? "" : "not "));
 
       mass_[i] = double_parameters_[i].second;
+      RCUTILS_LOG_INFO_NAMED(logger_name_.c_str(), "Mass for the axis %zu is %e", i, mass_[i]);
+      stiffness_[i] = double_parameters_[i + 6].second;
       RCUTILS_LOG_INFO_NAMED(
-        logger_name_.c_str(),
-       "Mass for the axis %zu is %e", i, mass_[i]);
-      stiffness_[i] = double_parameters_[i+6].second;
+        logger_name_.c_str(), "Stiffness for the axis %zu is %e", i, stiffness_[i]);
+      damping_[i] = double_parameters_[i + 12].second;
       RCUTILS_LOG_INFO_NAMED(
-        logger_name_.c_str(),
-       "Stiffness for the axis %zu is %e", i, stiffness_[i]);
-      damping_[i] = double_parameters_[i+12].second;
+        logger_name_.c_str(), "Damping for the axis %zu is %e", i, damping_[i]);
+      damping_ratio_[i] = double_parameters_[i + 18].second;
       RCUTILS_LOG_INFO_NAMED(
-        logger_name_.c_str(),
-        "Damping for the axis %zu is %e", i, damping_[i]);
-      damping_ratio_[i] = double_parameters_[i+18].second;
-      RCUTILS_LOG_INFO_NAMED(
-        logger_name_.c_str(),
-       "Damping_ratio for the axis %zu is %e", i, damping_ratio_[i]);
+        logger_name_.c_str(), "Damping_ratio for the axis %zu is %e", i, damping_ratio_[i]);
     }
 
     convert_damping_ratio_to_damping();
@@ -355,7 +340,6 @@ public:
   std::array<double, 6> stiffness_;
 };
 
-
 class AdmittanceRule
 {
 public:
@@ -368,33 +352,27 @@ public:
   controller_interface::return_type update(
     const trajectory_msgs::msg::JointTrajectoryPoint & current_joint_state,
     const geometry_msgs::msg::Wrench & measured_wrench,
-    const geometry_msgs::msg::PoseStamped & reference_pose,
-    const rclcpp::Duration & period,
-    trajectory_msgs::msg::JointTrajectoryPoint & desired_joint_states
-  );
+    const geometry_msgs::msg::PoseStamped & reference_pose, const rclcpp::Duration & period,
+    trajectory_msgs::msg::JointTrajectoryPoint & desired_joint_states);
 
   controller_interface::return_type update(
     const trajectory_msgs::msg::JointTrajectoryPoint & current_joint_state,
     const geometry_msgs::msg::Wrench & measured_wrench,
-    const std::array<double, 6> & reference_joint_deltas,
-    const rclcpp::Duration & period,
-    trajectory_msgs::msg::JointTrajectoryPoint & desired_joint_states
-  );
+    const std::array<double, 6> & reference_joint_deltas, const rclcpp::Duration & period,
+    trajectory_msgs::msg::JointTrajectoryPoint & desired_joint_states);
 
   controller_interface::return_type update(
     const trajectory_msgs::msg::JointTrajectoryPoint & current_joint_state,
     const geometry_msgs::msg::Wrench & measured_wrench,
     const geometry_msgs::msg::PoseStamped & reference_pose,
-    const geometry_msgs::msg::WrenchStamped & reference_force,
-    const rclcpp::Duration & period,
-    trajectory_msgs::msg::JointTrajectoryPoint & desired_joint_states
-  );
+    const geometry_msgs::msg::WrenchStamped & reference_force, const rclcpp::Duration & period,
+    trajectory_msgs::msg::JointTrajectoryPoint & desired_joint_states);
 
   controller_interface::return_type get_controller_state(
-    control_msgs::msg::AdmittanceControllerState & state_message
-  );
+    control_msgs::msg::AdmittanceControllerState & state_message);
 
-  controller_interface::return_type get_pose_of_control_frame_in_base_frame(geometry_msgs::msg::PoseStamped & pose);
+  controller_interface::return_type get_pose_of_control_frame_in_base_frame(
+    geometry_msgs::msg::PoseStamped & pose);
 
 public:
   // TODO(destogl): Add parameter for this
@@ -414,26 +392,19 @@ public:
   std::unique_ptr<filters::FilterChain<geometry_msgs::msg::WrenchStamped>> filter_chain_;
 
 protected:
-  void process_wrench_measurements(
-    const geometry_msgs::msg::Wrench & measured_wrench
-  );
+  void process_wrench_measurements(const geometry_msgs::msg::Wrench & measured_wrench);
 
   /**
    * All values are in he controller frame
    */
   void calculate_admittance_rule(
-    const std::array<double, 6> & measured_wrench,
-    const std::array<double, 6> & pose_error,
-    const rclcpp::Duration & period,
-    std::array<double, 6> & desired_relative_pose
-  );
+    const std::array<double, 6> & measured_wrench, const std::array<double, 6> & pose_error,
+    const rclcpp::Duration & period, std::array<double, 6> & desired_relative_pose);
 
   controller_interface::return_type calculate_desired_joint_state(
     const trajectory_msgs::msg::JointTrajectoryPoint & current_joint_state,
-    const std::array<double, 6> & relative_pose,
-    const rclcpp::Duration & period,
-    trajectory_msgs::msg::JointTrajectoryPoint & desired_joint_state
-  );
+    const std::array<double, 6> & relative_pose, const rclcpp::Duration & period,
+    trajectory_msgs::msg::JointTrajectoryPoint & desired_joint_state);
 
   // IK variables
   std::shared_ptr<MoveItKinematics> ik_;
@@ -499,50 +470,56 @@ protected:
   trajectory_msgs::msg::JointTrajectoryPoint admittance_rule_calculated_values_;
 
 private:
-  template<typename MsgType>
-  controller_interface::return_type
-  transform_to_control_frame(const MsgType & message_in, MsgType & message_out)
+  template <typename MsgType>
+  controller_interface::return_type transform_to_control_frame(
+    const MsgType & message_in, MsgType & message_out)
   {
     return transform_to_frame(message_in, message_out, parameters_.control_frame_);
   }
 
-  template<typename MsgType>
-  controller_interface::return_type
-  transform_to_ik_base_frame(const MsgType & message_in, MsgType & message_out)
+  template <typename MsgType>
+  controller_interface::return_type transform_to_ik_base_frame(
+    const MsgType & message_in, MsgType & message_out)
   {
     return transform_to_frame(message_in, message_out, parameters_.ik_base_frame_);
   }
 
-  template<typename MsgType>
-  controller_interface::return_type
-  transform_to_frame(const MsgType & message_in, MsgType & message_out, const std::string & frame)
+  template <typename MsgType>
+  controller_interface::return_type transform_to_frame(
+    const MsgType & message_in, MsgType & message_out, const std::string & frame)
   {
-    if (frame != message_in.header.frame_id) {
-      try {
-        geometry_msgs::msg::TransformStamped transform = tf_buffer_->lookupTransform(
-          frame, message_in.header.frame_id, tf2::TimePointZero);
+    if (frame != message_in.header.frame_id)
+    {
+      try
+      {
+        geometry_msgs::msg::TransformStamped transform =
+          tf_buffer_->lookupTransform(frame, message_in.header.frame_id, tf2::TimePointZero);
         tf2::doTransform(message_in, message_out, transform);
-      } catch (const tf2::TransformException & e) {
+      }
+      catch (const tf2::TransformException & e)
+      {
         RCLCPP_ERROR_SKIPFIRST_THROTTLE(
           rclcpp::get_logger("AdmittanceRule"), *clock_, 5000, "%s", e.what());
         return controller_interface::return_type::ERROR;
       }
-    } else {
+    }
+    else
+    {
       message_out = message_in;
     }
     return controller_interface::return_type::OK;
   }
 
-  template<typename MsgType>
-  controller_interface::return_type
-  transform_relative_to_control_frame(const MsgType & message_in, MsgType & message_out)
+  template <typename MsgType>
+  controller_interface::return_type transform_relative_to_control_frame(
+    const MsgType & message_in, MsgType & message_out)
   {
     return transform_relative_to_frame(message_in, message_out, parameters_.control_frame_);
   }
 
-  template<typename MsgType>
-  controller_interface::return_type
-  transform_relative_to_ik_base_frame(const MsgType & message_in, MsgType & message_out)
+  template <typename MsgType>
+  controller_interface::return_type transform_relative_to_ik_base_frame(
+    const MsgType & message_in, MsgType & message_out)
   {
     return transform_relative_to_frame(message_in, message_out, parameters_.ik_base_frame_);
   }
@@ -553,15 +530,15 @@ private:
    *   - https://answers.ros.org/question/192273/how-to-implement-velocity-transformation/?answer=192283#post-id-192283
    *   - https://physics.stackexchange.com/questions/197009/transform-velocities-from-one-frame-to-an-other-within-a-rigid-body#244364
    */
-  template<typename MsgType>
-  controller_interface::return_type
-  transform_relative_to_frame(const MsgType & message_in, MsgType & message_out, const std::string & frame)
+  template <typename MsgType>
+  controller_interface::return_type transform_relative_to_frame(
+    const MsgType & message_in, MsgType & message_out, const std::string & frame)
   {
     controller_interface::return_type ret = controller_interface::return_type::OK;
 
     // Do calculation only if transformation needed
-    if (frame != message_in.header.frame_id) {
-
+    if (frame != message_in.header.frame_id)
+    {
       MsgType message_transformed;
       std::array<double, 6> message_transformed_arr;
 
@@ -578,31 +555,35 @@ private:
       ret = transform_to_frame(zero_pose, zero_pose_transformed, frame);
       convert_message_to_array(zero_pose_transformed, zero_pose_transformed_arr);
 
-      for (auto i = 0u; i < 6; ++i) {
+      for (auto i = 0u; i < 6; ++i)
+      {
         relative_message_transformed_arr[i] =
           message_transformed_arr[i] - zero_pose_transformed_arr[i];
 
-        if (i >= 3) {
+        if (i >= 3)
+        {
           relative_message_transformed_arr[i] =
             angles::normalize_angle(relative_message_transformed_arr[i]);
         }
 
-        if (std::fabs(relative_message_transformed_arr[i]) < POSE_ERROR_EPSILON) {
+        if (std::fabs(relative_message_transformed_arr[i]) < POSE_ERROR_EPSILON)
+        {
           relative_message_transformed_arr[i] = 0.0;
         }
       }
       message_out.header = message_transformed.header;
       convert_array_to_message(relative_message_transformed_arr, message_out);
-    } else {
+    }
+    else
+    {
       message_out = message_in;
     }
 
     return ret;
   }
 
-  template<typename Type>
-  void
-  direct_transform(const Type & input, const tf2::Transform & transform, Type & output)
+  template <typename Type>
+  void direct_transform(const Type & input, const tf2::Transform & transform, Type & output)
   {
     // use TF2 data types for easy math
     tf2::Transform input_tf, output_tf;
